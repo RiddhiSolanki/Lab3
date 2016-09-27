ea96670f904bf21e80b0b7ab7c34911f8004b4f6
RiddhiSolanki <riddhisolanki91@gmail.com>
2016-09-27 16:14:06 -0400

    Lab 3

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!");
+            string cont;
+            do
+            {
+                Console.WriteLine("\n Enter an integer");
+                int userResponse = int.Parse(Console.ReadLine());
+
+
+                int Square,  Cube;
+                Console.WriteLine("\tNumber\tSquare\tCube");
+                //loop 10 times calculating and displaying values
+                for (int Number = 1; Number <= userResponse;
+               Number++)
+
+                {
+
+                    Square = Number * Number;
+
+                    Cube = Square * Number;
+
+                    Console.WriteLine("\t{0}\t{1}\t{2}", Number, Square, Cube);
+
+                }
+                // end of calculate and display
+                Console.WriteLine("Do you want to continue? y/n");
+                cont = Console.ReadLine();
+
+            }  while (cont == "y");
+
+            string EndingMessage = ("Thank you for selecting an integer. Goodbye");
+            Console.WriteLine(EndingMessage);
+
+            Console.ReadKey();
+        }
+    }
+}
